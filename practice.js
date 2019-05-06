9e1d5b710686e9426c419db77c7223fb1d715bab
Daniel Hayes <danhayes3008@outlook.com>
2019-05-06 11:19:14 +0000

    inital commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+var password = "ac11p5";
+var userName = "Hayes3008";
+
+function SignIn(login){
+    if (password == true){
+        console.log("Password Accepted");
+    }
+    
+    if(userName == true){
+        console.log("username accepted");
+    }
+}
+console.log();
